b8e0120b6b7164b6ef694c232ce95fc30f2819c2
Matthew DeGraffenreid <matthew.d@thoughtworks.com>
2020-04-21 17:49:26 -0400

    game updates

--- model/game.model.js
+++ model/game.model.js
@@ -2,7 +2,7 @@ const connection = require('../util/connection.js');
 const query = require('../util/query');
 
 const dbConfig = require('../dbConfig'); 
-const initScript = require('../init.sql');
+const initScript = require('../init.js');
 
 var Game = function(game){
     this.game = game;
@@ -13,3 +13,5 @@ Game.init = async function (result){
     const conn = await connection(dbConfig).catch(e => {}) 
     await query(conn, initScript, result).catch(console.log);
 }
+
+module.exports= Game;
