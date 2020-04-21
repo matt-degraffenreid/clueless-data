7dd23a04488d41a84933b56f3db7129ca76b693a
Matthew DeGraffenreid <matthew.d@thoughtworks.com>
2020-04-21 17:31:03 -0400

    enable multiple statements

--- util/connection.js
+++ util/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 
 module.exports = async (params) => new Promise(
 (resolve, reject) => {
-	const connection = mysql.createConnection({ host: 'localhost', user: 'root', password: '', database: 'clueless' });
+	const connection = mysql.createConnection({ host: 'localhost', user: 'root', password: '', database: 'clueless',multipleStatements: true });
   connection.connect(error => {
 	  if (error) {
           console.log('no connection');
